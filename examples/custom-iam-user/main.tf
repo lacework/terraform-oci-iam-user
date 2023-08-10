0a5368ec61979737510077973bfb884b8c7065b6
Kolbeinn <kolbeinn.karlsson@lacework.net>
2023-08-10 10:21:44 +0000

    style: run tf fmt on the repo (#9)
    Signed-off-by: Kolbeinn Karlsson <kolbeinn.karlsson@lacework.net>

--- examples/custom-iam-user/main.tf
+++ examples/custom-iam-user/main.tf
@@ -1,11 +1,11 @@
 module "lacework_iam_user" {
-  source = "../.."
-  tenancy_id = var.tenancy_ocid
-  email = "example@example.com"
-	name_prefix = "lacework_test"
+  source      = "../.."
+  tenancy_id  = var.tenancy_ocid
+  email       = "example@example.com"
+  name_prefix = "lacework_test"
 }
 
 variable "tenancy_ocid" {
-  type = string
+  type        = string
   description = "OCID of the tenancy to be integrated with Lacework"
 }
